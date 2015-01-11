66ff316e5fecab2b5e9f9d1216cbdbb2377085b4
Craig Comberbach <craigc@price-hvac.com>
2015-01-10 23:21:29 -0600

    Advanced working functionality
    Added full scanning functionality
    Added format/pre/post/finished function pointers to extend capabilities
    to encompass non-generic situations
    Added DSP based resolution increasing capabilities (As much as 16-bit
    can be obtained from the 10-bit A2D module)

--- A2D.c
+++ A2D.c
@@ -1,15 +1,13 @@
 /**************************************************************************************************
-Authours:				Craig Comberbach
 Target Hardware:		PIC24F
-Chip resources used:	A2D Module
-Code assumptions:
-Purpose:				Read the Thermistor inputs
+Chip resources used:	A2D Module & Pins setup as analog inputs
+Purpose:				Scan A2D, perform DSP to increase resolution, and format accordingly
 
 Version History:
 v0.0.0	2013-07-18  Craig Comberbach
 	Compiler: C30 v3.31	IDE: MPLABx 1.80	Tool: RealICE	Computer: Intel Xeon CPU 3.07 GHz, 6 GB RAM, Windows 7 64 bit Professional SP1
 	First version
-**************************************************************************************************/
+ **************************************************************************************************/
 /*************    Header Files    ***************/
 #include "Config.h"
 #include "A2D.h"
@@ -25,102 +23,364 @@ v0.0.0	2013-07-18  Craig Comberbach
 
 /************Arbitrary Functionality*************/
 /*************   Magic  Numbers   ***************/
+#define	NUMBER_OF_CHANNELS 16
+#define SCAN_BUFFER_SIZE	16	//Size of the scan buffer
+
 /*************    Enumeration     ***************/
-/***********State Machine Definitions*************/
-/*************  Global Variables  ***************/
-/*************Function  Prototypes***************/
-int bee_boop(void);
+/*************ArbitraryFunctionality*************/
+#define MAX_SCAN_QUEUE_SIZE	16	//Max size of the scan queue
 
-/************* Device Definitions ***************/
 /************* Module Definitions ***************/
+#define	STOP_SCAN		AD1CON1bits.ASAM=0	//Stops the scanning of channels
+#define	START_SCAN		AD1CON1bits.ASAM=1	//Starts the scanning of channels
+
 /************* Other  Definitions ***************/
-
-void Initialize_A2D(void)
+/*************  Global Variables  ***************/
+volatile char scanningQueue[MAX_SCAN_QUEUE_SIZE]; //The list of channels to scan, as well as the order to scan them in
+volatile char scanIsComplete;
+volatile char currentQueueElement;
+struct A2D_Channel_Attributes
 {
-	#if defined __PIC24F08KL200__
-		//Analog Input Configuration Register
-		ANCFGbits.VBGEN		= 0;//0 = Band gap is not available to the A/D Converter
+	unsigned char bitsOfResolutionIncrease;	//The number of bit of increased resolution (Default is 0 which is 10 bits)
+	unsigned int samplesRequired;			//The number of samples required to initate an averaging event (includes number of finished samples to make a final averaged sample)
+	unsigned int valueForDSP;				//Used as part of calculation to get the current value, includes what is required for resolution increasing as well as averaging
+	unsigned int samplesTaken;				//Current number of samples, in multiples of 16 (because samples are taken in bursts of 16)
+	int value;								//The most current averaged value, includes resolution increase if used
+	int (*formatPointer)(int);				//Used to specify a function that handles the formating of the averaged value
+	void (*preFunction)(int);				//Used to specify a function that activates when a channel starts being scanned (eg for setting a switched pin)
+	void (*postFunction)(int);				//Used to specify a function that activates when a channel stops being scanned (eg for resetting a switched pin)
+	void (*finishedFunction)(int);			//Used to specify a function that activates when a channel finishes being scanned and a new value is created (eg For setting flags for functions that need to run as soon as a value is determined)
+	unsigned long sumOfSamples;				//Sum of all the A2D samples before it undergoes DSP/Averaging
+} A2D_Channel[NUMBER_OF_CHANNELS];
 
-		//A/D Input Scan Select Register
-		AD1CSSLbits.CSSL0	= 0;//0 = Analog channel omitted from input scan
-		AD1CSSLbits.CSSL1	= 0;//0 = Analog channel omitted from input scan
-//		AD1CSSLbits.CSSL2	= Unimplemented on this chip
-//		AD1CSSLbits.CSSL3	= Unimplemented on this chip
-//		AD1CSSLbits.CSSL4	= Unimplemented on this chip
-//		AD1CSSLbits.CSSL6	= Unimplemented on this chip (But not specified as such)
-//		AD1CSSLbits.CSSL7	= Unimplemented on this chip (But not specified as such)
-//		AD1CSSLbits.CSSL8	= Unimplemented on this chip (But not specified as such)
-		AD1CSSLbits.CSSL9	= 0;//0 = Analog channel omitted from input scan
-		AD1CSSLbits.CSSL10	= 0;//0 = Analog channel omitted from input scan
-//		AD1CSSLbits.CSSL11	= Unimplemented on this chip
-//		AD1CSSLbits.CSSL12	= Unimplemented on this chip
-		AD1CSSLbits.CSSL13	= 1;//1 = Corresponding analog channel selected for input scan
-		AD1CSSLbits.CSSL14	= 1;//1 = Corresponding analog channel selected for input scan
-		AD1CSSLbits.CSSL15	= 0;//0 = Analog channel omitted from input scan
-
-		//A/D Input Select Register
-		AD1CHSbits.CH0SA	= 6;//0110 = Internal band gap reference (VBG)
-		AD1CHSbits.CH0NA	= 0;//0 = Channel 0 negative input is VR
-		AD1CHSbits.CH0SB	= 6;//0110 = Internal band gap reference (VBG)
-		AD1CHSbits.CH0NB	= 0;//0 = Channel 0 negative input is VR-
-
-		//A/D Control Register 3
-//		AD1CON3bits.ADCS	= Using internal RC clock, not main clock
-		AD1CON3bits.SAMC	= 13;//01101 = 13 TAD(TAD is the clock period)
-		AD1CON3bits.PUMPEN	= 0;//0 = Charge pump for switches is disabled
-		AD1CON3bits.EXTSAM	= 0;//0 = A/D is finished sampling
-		AD1CON3bits.ADRC	= 1;//1 = A/D internal RC clock
-
-		//A/D Control Register 2
-		AD1CON2bits.ALTS	= 0;//0 = Always uses MUX A input multiplexer settings
-		AD1CON2bits.SMPI	= 0;//1= Interrupts at the completion of conversion for each 2nd sample/convert sequence
-		AD1CON2bits.CSCNA	= 1;//1 = Scan inputs
-		AD1CON2bits.OFFCAL	= 0;//0 = Conversions to get the actual input value
-		AD1CON2bits.VCFG	= 0;//00 = Vr+ = AVdd, VR- = AVss
-
-		//A/D Control Register 1
-//		AD1CON1bits.DONE	= Not used
-//		AD1CON1bits.SAMP	= Not used
-		AD1CON1bits.ASAM	= 1;//1 = Sampling begins immediately after the last conversion completes; SAMP bit is auto-set
-		AD1CON1bits.SSRC	= 7;//111 = Internal counter ends sampling and starts conversion (auto-convert)
-		AD1CON1bits.FORM	= 0;//00 = Integer (0000 00dd dddd dddd)
-		AD1CON1bits.ADSIDL	= 0;//0 = Continue module operation in Idle mode
-		AD1CON1bits.ADON	= 1;//1 = A/D Converter module is operating
-bee_boop();
-	#endif
-
-	return;
-}
+/*************Function  Prototypes***************/
+int Change_To_Analog(int pin);
+int Change_To_Digital(int pin);
+int Add_To_Scan(int pin);
+int Remove_From_Scan(int pin);
+int Find_Next_Queue_Element(int channel);
+void __attribute__((__interrupt__, auto_psv)) _ADC1Interrupt(void);
 
 void A2D_Routine(void)
 {
-	int A2DValue[2];
+	int channel;
 
-	AD1CON1bits.ASAM = 1;
-	while(!IFS0bits.AD1IF);//wait for conversion to be done
-	IFS0bits.AD1IF = 0;
-	AD1CON1bits.ASAM = 0;
+	if(scanIsComplete)
+	{
+		//Reset for next time
+		scanIsComplete = 0;
 
-	A2DValue[0] = ADC1BUF0;
-	A2DValue[1] = ADC1BUF1;
+		//Add all of the samples into the raw variable
+		for(channel = 0; channel < NUMBER_OF_CHANNELS; channel++)
+			A2D_Channel[scanningQueue[currentQueueElement]].sumOfSamples += *(&ADC1BUF0 + channel);
 
-	Nop();
-	Nop();
-	Nop();
+		//Increment the number of samples read in (we just took 16 samples in a burst, hence the +16)
+		A2D_Channel[scanningQueue[currentQueueElement]].samplesTaken += SCAN_BUFFER_SIZE;
+
+		//Check if we are ready for DSP/averaging
+		if(A2D_Channel[scanningQueue[currentQueueElement]].samplesTaken >= A2D_Channel[scanningQueue[currentQueueElement]].samplesRequired)
+		{
+			//Perform the DSP/averaging
+			A2D_Channel[scanningQueue[currentQueueElement]].sumOfSamples /= A2D_Channel[scanningQueue[currentQueueElement]].valueForDSP; //Create average DSP value
+
+			//Apply formats externaly if required
+			if(*A2D_Channel[scanningQueue[currentQueueElement]].formatPointer == NO_FORMATING)
+				A2D_Channel[scanningQueue[currentQueueElement]].value = (int)A2D_Channel[scanningQueue[currentQueueElement]].sumOfSamples;
+			else
+				A2D_Channel[scanningQueue[currentQueueElement]].value = A2D_Channel[scanningQueue[currentQueueElement]].formatPointer((int)A2D_Channel[scanningQueue[currentQueueElement]].sumOfSamples);
+
+			//House keeping - Reset the counter and storage variable
+			A2D_Channel[scanningQueue[currentQueueElement]].samplesTaken = 0;
+			A2D_Channel[scanningQueue[currentQueueElement]].sumOfSamples = 0;
+
+			//Perform the new reading function action if applicable
+			if(*A2D_Channel[scanningQueue[currentQueueElement]].finishedFunction != NO_FINISHED_FUNCTION)
+				A2D_Channel[scanningQueue[currentQueueElement]].finishedFunction(scanningQueue[currentQueueElement]);
+		}
+
+		//Remove the current channel from the scanning
+		Remove_From_Scan(scanningQueue[currentQueueElement]);
+
+		//Move onto the next channel to be scanned
+		currentQueueElement = Find_Next_Queue_Element(currentQueueElement);
+
+		//Add the next channel to the scan
+		Add_To_Scan(scanningQueue[currentQueueElement]);
+	}
+
+	//Perform the beginning of scan action if applicable
+	if(*A2D_Channel[scanningQueue[currentQueueElement]].preFunction != NO_PREFUNCTION)
+		A2D_Channel[scanningQueue[currentQueueElement]].preFunction(scanningQueue[currentQueueElement]);
+
+	//(Re)start the Automatic scanning of the Analog ports
+	START_SCAN;
 
 	return;
 }
 
-int bee_boop(void)
+int Find_Next_Queue_Element(int channel)
 {
-	ANSAbits.ANSA0 = 0;
-	ANSAbits.ANSA1 = 0;
-	ANSAbits.ANSA2 = 1;
-	ANSAbits.ANSA3 = 1;
+	int nextChannel = channel;
 
-	ANSBbits.ANSB4 = 0;
-	ANSBbits.ANSB14 = 0;
-	ANSBbits.ANSB15 = 0;
+	//Advance to the next valid queue item - Do while used to ensure it runs at least once
+	do
+	{
+		++nextChannel;
+		if(nextChannel >= MAX_SCAN_QUEUE_SIZE)
+			nextChannel = 0;
+	}
+	while((scanningQueue[nextChannel] == -1) && (nextChannel != channel));//Find the next channel to scan, or we end up back where we started
 
+	return nextChannel;
+}
+
+void __attribute__((__interrupt__, auto_psv)) _ADC1Interrupt(void)
+{
+	//Clear interrupt flag
+	IFS0bits.AD1IF = 0;
+
+	//Temporarily turn off automatic scanning until we figure out what to do with our current samples
+	STOP_SCAN;
+
+	//Perform the end of scan action if applicable
+	if(*A2D_Channel[scanningQueue[currentQueueElement]].postFunction != NO_POSTFUNCTION)
+		A2D_Channel[scanningQueue[currentQueueElement]].postFunction(scanningQueue[currentQueueElement]);
+
+	//Let the A2D routine know that we have finished
+	scanIsComplete = 1;
+
+	return;
+}
+
+void A2D_Initialize(void)
+{
+	int channel;
+
+	//Initialize scan queue
+	for(channel = 0; channel < MAX_SCAN_QUEUE_SIZE; ++channel)
+		scanningQueue[channel] = -1;//Unassigned
+
+	//AD1 Interrupt
+	IFS0bits.AD1IF = 0;				//0 = Interrupt request has not occurred
+	IEC0bits.AD1IE = 1;				//1 = Interrupt request is enabled
+
+	//A/D Input Scan Select Register (Low)
+	AD1CSSL = 0;					//0 = Analog channel omitted from input scan
+
+	//A/D Port Configuration Register
+	AD1PCFG = ~0;					//1 = Pin for corresponding analog channel is configured in Digital mode; I/O port read is enabled
+
+	//A/D Input Select Register
+	AD1CHSbits.CH0SA = 0b0111;		//0111 = AVDD
+	AD1CHSbits.CH0NA = 0;			//0 = Channel 0 negative input is VR-
+	AD1CHSbits.CH0SB = 0b0111;		//0111 = AVDD
+	AD1CHSbits.CH0NB = 0;			//0 = Channel 0 negative input is VR-
+
+	//A/D Control Register 3
+	AD1CON3bits.ADCS = 0b111111;	//111111 = 64 * TCY
+	AD1CON3bits.SAMC = 0b11111;		//11111 = 31 TAD
+	AD1CON3bits.ADRC = 1;			//1 = A/D internal RC clock
+
+	//A/D Control Register 2
+	AD1CON2bits.ALTS = 0;			//0 = Always uses MUX A input multiplexer settings
+	AD1CON2bits.BUFM = 0;			//0 = Buffer is configured as one 16-word buffer (ADC1BUFn<15:0>)
+	AD1CON2bits.SMPI = 0b1111;		//1111 = Interrupts at the completion of conversion for each 16th sample/convert sequence
+	//AD1CON2bits.BUFS is Read Only
+	AD1CON2bits.CSCNA = 1;			//1 = Scan inputs
+	AD1CON2bits.OFFCAL = 0;			//0 = Converts to get the actual input value
+	AD1CON2bits.VCFG = 0;			//0 = Vr+ = AVDD, Vr- = AVSS
+
+	//A/D Control Register 1
+//	AD1CON1bits.DONE is Read Only
+//	AD1CON1bits.SAMP is Read Only
+	AD1CON1bits.ASAM = 0;		//0 = Sampling begins when SAMP bit is set
+	AD1CON1bits.SSRC = 0b111;	//111 = Internal counter ends sampling and starts conversion (auto-convert)
+	AD1CON1bits.FORM = 0;		//0 = Integer (0000 00dd dddd dddd)
+	AD1CON1bits.ADSIDL = 1;		//1 = Discontinue module operation when device enters Idle mode
+	AD1CON1bits.ADON = 1;		//1 = A/D Converter module is operating
+
+	return;
+}
+
+int Add_To_Scan(int pin)
+{
+	//Range checking
+	if(pin >= NUMBER_OF_CHANNELS)
+		return 0;//Failure
+
+	//Choose the correct pin
+	pin = 1 << pin;
+
+	//Turn off the module, changing a CSSL bit with it on can lead to issues
+	AD1CON1bits.ADON = 0;
+
+	//Set the pin to be scanned
+	AD1CSSL |= pin;
+
+	//Turn the module back on
+	AD1CON1bits.ADON = 1;
+
+	return 1;//Success
+}
+
+int Remove_From_Scan(int pin)
+{
+	//Range checking
+	if(pin >= NUMBER_OF_CHANNELS)
+		return 0;//Failure
+
+	//Choose the correct pin
+	pin = 1 << pin;
+
+	//Turn off the module, changing a CSSL bit with it on can lead to issues
+	AD1CON1bits.ADON = 0;
+
+	//Remove the pin from being scanned
+	AD1CSSL &= ~pin;
+
+	//Turn the module back on
+	AD1CON1bits.ADON = 1;
+
+	return 1;//Success
+}
+
+int Change_To_Analog(int pin)
+{
+	//Range checking
+	if(pin >= NUMBER_OF_CHANNELS)
+		return 0;//Failure
+
+	//Choose the correct pin
+	pin = 1 << pin;
+
+	//Set the pin(s) to digital
+	AD1PCFG &= ~pin;
+
+	return 1;//Success
+}
+
+int Change_To_Digital(int pin)
+{
+	//Range checking
+	if(pin >= NUMBER_OF_CHANNELS)
+		return 0;//Failure
+
+	//Choose the correct pin
+	pin = 1 << pin;
+
+	//Set the pin(s) to digital
+	AD1PCFG |= pin;
+
+	return 1;//Success
+}
+
+int A2D_Add_To_Scan_Queue(int channel)
+{
+	int position = 0;
+
+	//Check if we are within a valid range of channels
+	if((channel < 0) || (channel >= NUMBER_OF_CHANNELS))
+		return 0;
+
+	//Find the next free scan position
+    while((scanningQueue[position] != -1) && (position < MAX_SCAN_QUEUE_SIZE))
+        position++;
+
+	//Make it so
+	if(position < MAX_SCAN_QUEUE_SIZE)
+		scanningQueue[position] = channel;//Make it official
+	else
+		return 0;
+
+	//So much win!
 	return 1;
+}
+
+int A2D_Channel_Settings(int channel, enum RESOLUTION desiredResolutionIncrease, int numberOfAverages, int (*formatPointer)(int), void (*preFunction)(int), void (*postFunction)(int), void (*finishedFunction)(int))
+{
+	unsigned long samplesRequired;
+
+	//Check if we are within a valid range of channels
+	if((channel < 0) || (channel >= NUMBER_OF_CHANNELS))
+		return 0;
+
+	//Determine the number of samples required
+	samplesRequired = numberOfAverages;
+	switch(desiredResolutionIncrease)
+	{
+		case RESOLUTION_10_BIT:
+			samplesRequired *= 1;//4^0
+			break;
+		case RESOLUTION_11_BIT:
+			samplesRequired *= 4;//4^1
+			break;
+		case RESOLUTION_12_BIT:
+			samplesRequired *= 16;//4^2
+			break;
+		case RESOLUTION_13_BIT:
+			samplesRequired *= 64;//4^3
+			break;
+		case RESOLUTION_14_BIT:
+			samplesRequired *= 256;//4^4
+			break;
+		case RESOLUTION_15_BIT:
+			samplesRequired *= 1024;//4^5
+			break;
+		case RESOLUTION_16_BIT:
+			samplesRequired *= 4096;//4^6
+			break;
+		default:
+			return 0;
+	}
+
+	//Check if we have a valid number of averages (Minimum of 16 and must be a multiple of 16 that is no higher than a 16 bit number)
+	if(((samplesRequired % 16) != 0) || (samplesRequired < 16) || (samplesRequired >= 65536))
+		return 0;
+
+	//Set values
+	A2D_Channel[channel].value = 0;
+	A2D_Channel[channel].sumOfSamples = 0;
+	A2D_Channel[channel].bitsOfResolutionIncrease = desiredResolutionIncrease;
+	A2D_Channel[channel].samplesRequired = (int)samplesRequired;
+	A2D_Channel[channel].samplesTaken = 0;
+	A2D_Channel[channel].formatPointer = formatPointer;
+	A2D_Channel[channel].preFunction = preFunction;
+	A2D_Channel[channel].postFunction = postFunction;
+	A2D_Channel[channel].finishedFunction = finishedFunction;
+	
+	//Calculate the DSP and averaging number
+	A2D_Channel[channel].valueForDSP = numberOfAverages;
+	switch(desiredResolutionIncrease)
+	{
+		case RESOLUTION_10_BIT:
+			break;
+		case RESOLUTION_11_BIT:
+			A2D_Channel[channel].valueForDSP *= 2;//2^1;
+			break;
+		case RESOLUTION_12_BIT:
+			A2D_Channel[channel].valueForDSP *= 4;//2^2;
+			break;
+		case RESOLUTION_13_BIT:
+			A2D_Channel[channel].valueForDSP *= 8;//2^3;
+			break;
+		case RESOLUTION_14_BIT:
+			A2D_Channel[channel].valueForDSP *= 16;//2^4;
+			break;
+		case RESOLUTION_15_BIT:
+			A2D_Channel[channel].valueForDSP *= 32;//2^5;
+			break;
+		case RESOLUTION_16_BIT:
+			A2D_Channel[channel].valueForDSP *= 64;//2^6;
+			break;
+	}
+
+	Change_To_Analog(channel);
+
+	//Success!
+	return 1;
+}
+
+int A2D_Value(int channel)
+{
+	return A2D_Channel[channel].value; //Return the value - NOTE: it has already been formatted
 }
--- A2D.h
+++ A2D.h
@@ -1,15 +1,134 @@
+/*
+ Instructions for adding to a new project:
+Add the code with the header "Add to config file header" to the config.h file, and the code labeled "Add to config file"
+to config.c. The analog pin definitions (A2D_PIN_DEFINITIONS) that are placed into config.h need to line up incrementally based
+on the analog channel (eg AN5 needs to be enumerated to 5).
+
+The initiliaze routine needs to be run once on startup. After that the you are required to run both A2D_Channel_Settings() and
+A2D_Add_To_Scan_Queue() before the A2D will work. Calling the A2D_Add_To_Scan_Queue() function several times adds multiple
+instances of the channel to the queue. This is a way to get a quicker update rate on a channel if there are several other
+channels. Running A2D_Channel_Settings() once a conversion is under way will reset the channel to the specified new settings and
+restart the conversion process. It would typically only be called multiple times if a different resolution was temporarily
+required, or if the format/pre/post/finished functions needed to be changed for use in different configurations.
+
+Reading the current value is done through A2D_Value(). Note: This value is not the raw A2D value, rather it is already formatted
+and at the correct resolution as specificied according to the latest calling of A2D_Channel_Settings();
+
+A2D_Routine() needs to be called on a regular basis. The more often it is called, the faster channels will update their values.
+Calling the routine before a conversion is done will not interrupt the current conversion, though it will have no other effect.
+
+The markup above each function will pop-up as a helpful reminder of the arguments each function will take, as well as what value
+is returned, and what the function will do.
+
+Calculating how much time an full update will take to complete is done via the following formulae:
+ * (4^b*s*q*t)/(16*r)
+ * Where:
+ * b = Bits of resolution increase from 10-bit (0 for 10 bit, 1 for 11 bit, etc)
+ * s = Number of samples at the requested resolution for an updated value
+ * q = Number of channels in the queue (NOTE: Includes repeated channels)
+ * t = The time interval between calling the A2D_Routine (Eg. Time of main loop)
+ * 16 = Constant, it represents the number of samples per scan
+ * r = Number of times a channel appears in the queue (Can be more than once for repeated channels)
+*/
+
 #ifndef A2D_H
-#define	A2D_H
+#define A2D_H
+
+/***********Add to config file header************/
+/*
+enum A2D_PIN_DEFINITIONS
+{
+	A2D_A0 = 0,		//A0
+	A2D_A1 = 1,		//A1
+	A2D_A2 = 2,		//A2
+	A2D_A3 = 3,		//A3
+	A2D_A4 = 4,		//A4
+	A2D_A5 = 5,		//A5
+	A2D_A6 = 6,		//A6
+	A2D_A7 = 7,		//A7
+	A2D_A8 = 8,		//A8
+	A2D_A9 = 9,		//A9
+	A2D_A10 = 10,	//A10
+	A2D_A11 = 11,	//A11
+	A2D_A12 = 12,	//A12
+	A2D_A13 = 13,	//A13
+	A2D_A14 = 14,	//A14
+	A2D_A15 = 15,	//A15
+};
+
+//A2D Library
+#define A2D_MAJOR	0
+#define A2D_MINOR	1
+#define A2D_PATCH	0
+*/
+
+/***************Add to config file***************/
+/*
+#ifndef A2D_LIBRARY
+	#error "You need to include the A2D library for this code to compile"
+#endif
+ */
 
 /************* Semantic Versioning***************/
 #define A2D_LIBRARY
 
 /*************   Magic  Numbers   ***************/
+#define NO_FORMATING			(void*)0
+#define NO_PREFUNCTION			(void*)0
+#define NO_POSTFUNCTION			(void*)0
+#define NO_FINISHED_FUNCTION	(void*)0
+
 /*************    Enumeration     ***************/
+enum RESOLUTION
+{
+	RESOLUTION_10_BIT,	//0 (Max samples = 65520)
+	RESOLUTION_11_BIT,	//1 (Max samples = 16380)
+	RESOLUTION_12_BIT,	//2 (Max samples = 4095)
+	RESOLUTION_13_BIT,	//3 (Max samples = 1023)
+	RESOLUTION_14_BIT,	//4 (Max samples = 255)
+	RESOLUTION_15_BIT,	//5 (Max samples = 63)
+	RESOLUTION_16_BIT	//6 (Max samples = 15)
+};
+
 /***********State Machine Definitions************/
 /*************Function  Prototypes***************/
-void Initialize_A2D(void);
+/**
+ * Used to setup the A2D Module
+ */
+void A2D_Initialize(void);
+
+/**
+ * Sets up the fundamental settings of the scan, allowing you to change the number of samples and increasing resolution, in addition to adding custom formating and inserting function before/after a scan as well as when a channel is finished scanning
+ * @param channel The A2D channel that is to be scanned, these are enumerated in the controller config file
+ * @param desiredResolution The desired additional bits of resolution (between 0(10-bit) and 6(16-bit)), you can use enum RESOLUTION in this header file to simplify
+ * @param numberOfAverages The number of desired "readings" to be averaged (This can be different than the number of samples taken if additional resolution is selected). This number must be 16 or greater, must not be 65536 or bigger and must be a multiple of 16.
+ * @param formatPointer Function pointer that will format the raw A2D values, the function must accept an integer indicating A2D channel, it should also return the formatted value as an integer
+ * @param preFunction Function pointer that will run just as the channel is starting to be scanned (eg switched pin turning on), the function must accept an integer indicating A2D channel, it should not return a value
+ * @param postFunction Function pointer that will run just as the channel is finished being scanned (eg switched pin turning off), the function must accept an integer indicating A2D channel, it should not return a value
+ * @param finishedFunction Function pointer that will run when the sampling/averaging is completed and a new value is ready (eg functions that need to run as soon as a sample is ready), the function must accept an integer indicating A2D channel, it should not return a value
+ * @return 1 = Channel was updated successfully, 0 = Value out of range, no changes were made
+ */
+int A2D_Channel_Settings(int channel, enum RESOLUTION desiredResolutionIncrease, int numberOfAverages, int (*formatPointer)(int), void (*preFunction)(int), void (*postFunction)(int), void (*finishedFunction)(int));
+
+/**
+ * Adds the channel to the scanning queue
+ * @param channel The channel that is to be added to the scanning queue, these are declared in the controller config file
+ * @return 1 = Success, 0 = Failure - Channel out of range
+ */
+int A2D_Add_To_Scan_Queue(int channel);
+
+/**
+ * Calculates and updates averaged/formatted result, through the magic of DSP it will also increase the resolution if required
+ * Calling this function multiple times will only do something if a conversion has completed
+ * Calling this function will start a conversion if it is not already converting
+  */
 void A2D_Routine(void);
 
-#endif	/* A2D_H */
+/**
+ * Returns the current value of the selected channel (Optionally formatted)
+ * @param channel The analog channel that you require the formatted value of, these are declared in the controller config file
+ * @return The formatted A2D values
+ */
+int A2D_Value(int channel);
 
+#endif
